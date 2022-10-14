2505aa5a326ff01f8912c1f8fb5637508c9cfd21
Restprojectuser <111348421+Restprojectuser@users.noreply.github.com>
2022-10-14 09:43:13 +0530

    initial commit

---
+++ src/test/java/com/Dockerdemo/demoDockerprac/DemoDockerpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dockerdemo.demoDockerprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
