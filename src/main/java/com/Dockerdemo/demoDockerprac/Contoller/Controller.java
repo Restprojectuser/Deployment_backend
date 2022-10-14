02bdc7b5bb96beed82c1e2d18742c9f700ae9369
naiyar imam <naiyar.imam@incture.com>
2022-10-14 09:57:24 +0530

    code fix

--- src/main/java/com/Dockerdemo/demoDockerprac/Contoller/Controller.java
+++ src/main/java/com/Dockerdemo/demoDockerprac/Contoller/Controller.java
@@ -1,7 +1,11 @@
+package com.Dockerdemo.demoDockerprac.Contoller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
     @RequestMapping("/getData")
     public String getData() {
--- src/main/java/com/Dockerdemo/demoDockerprac/DemoDockerpracApplication.java
+++ src/main/java/com/Dockerdemo/demoDockerprac/DemoDockerpracApplication.java
@@ -2,8 +2,9 @@ package com.Dockerdemo.demoDockerprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemoDockerpracApplication {
 
 	public static void main(String[] args) {
